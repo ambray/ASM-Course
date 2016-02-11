6a6cce70bb1b36dc7f837552f7a5114704c5bc98
aaron <aaron@relict.local>
2016-02-10 18:28:04 -0600

    Fixed an issue with asm file

--- labs/Lab1/Lab1.nasm
+++ labs/Lab1/Lab1.nasm
@@ -1,6 +1,7 @@
-bits 32
+bits 64
 
 global _first_func
 
 _first_func:
+	mov rax, 0x10
 	ret
--- labs/Lab1/main.cpp
+++ labs/Lab1/main.cpp
@@ -6,6 +6,6 @@ extern "C" int first_func(void);
 
 int main(int argc, char** argv)
 {
-
+	printf("Return: %d\n", first_func());
 	return 0;
 }
