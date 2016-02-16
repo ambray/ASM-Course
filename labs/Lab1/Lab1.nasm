3f00b3ab42dfdbcba72188aaa7e11accd38c0c66
aaron <aaron@relict.local>
2016-02-16 10:27:35 -0600

    Added first test instructions and value checking.

--- labs/Lab1/Lab1.nasm
+++ labs/Lab1/Lab1.nasm
@@ -3,5 +3,17 @@ bits 64
 global _first_func
 
 _first_func:
-	mov rax, 0x10
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;  This is your first method for
+;  the lab. First problem:
+;  move the number 16 into
+;  the register RAX.
+;
+;  BEGIN student code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	mov rax, 0x20
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;  END student code.
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	ret
--- labs/Lab1/main.cpp
+++ labs/Lab1/main.cpp
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 extern "C" int first_func(void);
+
+#define EQ(x,y)\
+do { if(x == y) printf("[*] Success! Values are equal!\n"); \
+else printf("[x] Test Failed! Value %d does not match expected value %d!\n", x, y); } while(0)
 
 int main(int argc, char** argv)
 {
-	printf("Return: %d\n", first_func());
+    printf("Preparing to run the first test (moving a value to a register)\n");
+    EQ(first_func(), 16);
+
 	return 0;
 }
